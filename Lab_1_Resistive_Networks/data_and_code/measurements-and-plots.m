33e3addc14779eb6b2d43e84289244ae15c8e928
Anusha Datar <anushadatar@gmail.com>
2019-02-06 09:40:10 -0500

    first day in lab

---
+++ Lab_1_Resistive_Networks/data_and_code/measurements-and-plots.m
@@ -0,0 +1,21 @@
+%% Experiment One  
+% Measured resistance of a single resitor by comparing change in current in
+% response to Voltage over time. Values stored in IoutExp1 and VinExp1
+
+% Plot points as points and line of best fit.
+
+%% Experiment Two :
+% Measured voltage at the center of a standard voltage divider and studied 
+% how it varied in response to change in voltage. 
+% Values stored in VinExp2 and VinExp2.
+
+
+
+%% Experiment Three :
+% Measured current at the center of a standard voltage divider and studied 
+% how it varied in response to change in current. 
+% Values stored in IinExp2 and IinExp2.
+
+
+% Experiment Four : 
+%%
