5e803d2ba4fd099ca53e60e78f1a5cb2365ffa93
Anusha Datar <anushadatar@gmail.com>
2019-02-12 16:38:02 -0500

    Data collection from 2/12/2019

---
+++ Lab_2_Resistors_and_Diodes/data_collection.m
@@ -0,0 +1,7 @@
+% Experiment One : Diode-Connected 
+    % Iin_Exp1_1 and Vout_Exp1_1 are from the first V-I characteristic
+    % Vin_Exp1_2 and Iout_Exp1_2 are from the first I-V characteristic
+% Experiment Two: Resistor in series w/ Transistor
+    % Channel 2 sources voltage and measuring currents
+    % Channel 1 is measuring voltage
+    % Variable names are based off of resistor values
